f7dea4762235baea0e27efbcfe766cf433247b9e
qedir314 <qedirrehmetov252@yahoo.com>
2024-08-27 20:52:37 +0400

    Added Jinja tags

--- app.py
+++ app.py
@@ -2,9 +2,40 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Software Engineer',
+        'location': 'Baku, Azerbaijan',
+        'salary': '2000 AZN',
+    },
+    {
+        'id': 2,
+        'title': 'Data Scientist',
+        'location': 'Baku, Azerbaijan',
+        'salary': '3000 AZN',
+    },
+    {
+        'id': 3,
+        'title': 'Product Manager',
+        'location': 'Ankara, Turkey',
+        'salary': '10000 TL'
+    },
+    {
+        'id': 4,
+        'title': 'Machine Learning Engineer',
+        'location': 'Paris, France',
+        'salary': '6000 EURO'
+    }
+]
+
+
 @app.route("/")
 def hello_world():
-    return render_template("home.html")
+    return render_template("home.html",
+                           jobs=JOBS,
+                           company_name="Jovian")
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
